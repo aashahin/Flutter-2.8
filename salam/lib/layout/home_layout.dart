f5b8b5355fbf2d28f38883508e01fec14a1b399a
Abdelrahman Shaheen <almhtrfahmed123@gmail.com>
2021-07-16 18:10:30 +0200

    Database sqflite create

--- salam/lib/layout/home_layout.dart
+++ salam/lib/layout/home_layout.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:salam/modules/archived_tasks/archived_tasks_screen.dart';
 import 'package:salam/modules/done_tasks/done_tasks_screen.dart';
 import 'package:salam/modules/new_tasks/new_tasks_screen.dart';
+import 'package:sqflite/sqflite.dart';
 
 class HomeLayout extends StatefulWidget {
-
   @override
   _HomeLayoutState createState() => _HomeLayoutState();
 }
@@ -21,6 +21,13 @@ class _HomeLayoutState extends State<HomeLayout> {
     'Done Tasks',
     'Archived Tasks'
   ];
+
+  @override
+  void initState() {
+    // TODO : DATABASE SQFLITE
+    super.initState();
+    createDatabase();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,7 +40,26 @@ class _HomeLayoutState extends State<HomeLayout> {
       body: screens[currentIndex],
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.deepPurpleAccent,
-        onPressed: (){},
+        onPressed: () async // نضع اسينك في حالة كان العنصر سيعمل في الخلفية ولا نضعها في الخاصية الثانية
+        {
+          // try{
+          //   var name = await getname(); // اويت أي انتظر في حالة كان يستلم بيانات فنعطيه هذه القيمة
+          //   print(name);
+          //   throw ('انشاء خطأ');
+          // } catch(error){ // رسالة الخطأ
+          //   print('error ${error.toString()}');
+          // }
+getname().then((value) {
+  var name = value;
+  print(name);
+  print('osama');
+  // الفرق بين الأول والثاني أن الأول قد تظهر النتيجية الثانيه في حال تاخرت الاولي بخللاف هذه تعرض كل في ترتيبه حتي وان تاخرت
+}
+).catchError((error){ // رسالة خطأ في حال فشل التنفيذ
+  print('error ${error.toString()}');
+}
+);
+        },
         child: Icon(Icons.add),
         hoverColor: Colors.deepPurple,
       ),
@@ -63,5 +89,30 @@ class _HomeLayoutState extends State<HomeLayout> {
         ],
       ),
     );
+  }
+  Future<String> getname() async //فيوتشر : عنصر يعمل في الخلفية خاص بقاعدة البيانات وارسالها
+  {
+    return 'Ahmed';
+  }
+  void createDatabase() async
+  {
+    var database = await openDatabase(
+      'todo.db',
+      version: 1,
+      onCreate:(database,version){
+        print('database created');
+        database.execute('CREATE TABLE tasks (id INTEGER PRIMARY KEY, title TEXT,date TEXT,time TEXT,status TEXT)').then((value) {
+          print('table created');
+        }).catchError((error){
+          print('Error When Creating Table ${error.toString()}');
+        });
+      },
+      onOpen: (database){
+        print('database opened');
+
+      }
+    );
+  }
+  void insertToDatabase(){
   }
 }
